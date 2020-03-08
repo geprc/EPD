7cf056fc4d1071c733d16f40d697caf89dc156df
geprc <geprc>
2020-03-08 22:54:28 +0800

    remove ESPAsyncWebSocket and OTA

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <GxEPD.h>
 #include <SPIFFS.h>
 #include <Arduino.h>
-#include <ArduinoOTA.h>
 #include <WebSocketsServer.h>
 #include <ESPAsyncWebServer.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
@@ -74,8 +73,6 @@ void setup() {
         display.updateWindow(0, 0, 200, 200, false);
         qrcode.create(WiFi.softAPIP().toString());
     }
-    // ws.onEvent(onEvent);
-    // server.addHandler(&ws);
     webSocket.onEvent(webSocketEvent);
     webSocket.begin();
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -87,41 +84,10 @@ void setup() {
     });
     server.serveStatic("/", SPIFFS, "/www/");
     server.begin();
-    ArduinoOTA
-        .onStart([]() {
-            String type;
-            if (ArduinoOTA.getCommand() == U_FLASH)
-                type = "sketch";
-            else  // U_SPIFFS
-                type = "filesystem";
-
-            // NOTE: if updating SPIFFS this would be the place to unmount
-            // SPIFFS using SPIFFS.end()
-            Serial.println("Start updating " + type);
-        })
-        .onEnd([]() { Serial.println("\nEnd"); })
-        .onProgress([](unsigned int progress, unsigned int total) {
-            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        })
-        .onError([](ota_error_t error) {
-            Serial.printf("Error[%u]: ", error);
-            if (error == OTA_AUTH_ERROR)
-                Serial.println("Auth Failed");
-            else if (error == OTA_BEGIN_ERROR)
-                Serial.println("Begin Failed");
-            else if (error == OTA_CONNECT_ERROR)
-                Serial.println("Connect Failed");
-            else if (error == OTA_RECEIVE_ERROR)
-                Serial.println("Receive Failed");
-            else if (error == OTA_END_ERROR)
-                Serial.println("End Failed");
-        });
-    ArduinoOTA.begin();
 }
 
 void loop() {
     webSocket.loop();
-    ArduinoOTA.handle();
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
